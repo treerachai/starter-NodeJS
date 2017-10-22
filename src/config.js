28f0af3da9a3e61d52973666727e8d32c7825a84
treerachai <32605493+treerachai@users.noreply.github.com>
2017-10-22 19:39:49 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,3 @@
+process.env.REQUEST_TOKEN = '14ba4a4821ccb2807cd599713ca17203'
+process.env.LANGUAGE = 'fr'
+process.env.PORT = '5000'
--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const bodyParser = require('body-parser')
 
 // Load configuration
 require('./config')
-const bot = require('./bot')
+const bot = require('./bot').bot
 
 // Start Express server
 const app = express()
@@ -25,17 +25,21 @@ app.use(bodyParser.json())
 
 // Handle / route
 app.use('/', (request, response) => {
-  bot.reply(request, response)
-    .then(success => {
-      console.log(success)
-      if (!response.headersSent) { response.status(200) }
-    }).catch(error => {
+
+  // Call bot main function
+  bot(request.body, response, (error, success) => {
+    if (error) {
       console.log('Error in your bot:', error)
       if (!response.headersSent) { response.sendStatus(400) }
-    })
+    } else if (success) {
+      console.log(success)
+      if (!response.headersSent) { response.status(200).json(success) }
+    }
+  })
+
 })
 
-if (!process.env.REQUEST_TOKEN) {
+if (!process.env.REQUEST_TOKEN.length) {
   console.log('ERROR: process.env.REQUEST_TOKEN variable in src/config.js file is empty ! You must fill this field with the request_token of your bot before launching your bot locally')
 
   process.exit(0)
